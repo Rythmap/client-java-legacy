aea6435bb648defd12caaf31acfac2fd94de99bf
qylq <dider90n2@gmail.com>
2024-01-23 19:43:28 +0300

    init

--- client/app/src/main/java/com/qylex/melomap/SecretData.java
+++
@@ -1,7 +0,0 @@
-package com.qylex.melomap;
-
-public class SecretData {
-    static String serverUrl = "https://melomap-production.up.railway.app/";
-    static String melomapApi = serverUrl + "api/melomap/";
-    static String musicApi = serverUrl + "api/music/";
-}
