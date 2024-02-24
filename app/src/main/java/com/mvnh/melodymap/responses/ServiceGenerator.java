aa84c8bd99920cce803854e8ff229adfbda76d18
13mvnh <dider90n2@gmail.com>
2024-02-24 20:36:09 +0300

    changed server url

--- app/src/main/java/com/mvnh/melodymap/responses/ServiceGenerator.java
+++ app/src/main/java/com/mvnh/melodymap/responses/ServiceGenerator.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
-    public static final String BASE_URL = "https://melomap-production.up.railway.app/";
+    public static final String BASE_URL = "https://melomap.fun/";
     private static Retrofit.Builder builder
             = new Retrofit.Builder()
             .baseUrl(BASE_URL)
