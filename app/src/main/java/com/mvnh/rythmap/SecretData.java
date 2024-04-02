087d233f2a36285065e7a40179b1c67d7e3749d5
13mvnh <dider90n2@gmail.com>
2024-04-02 20:56:58 +0300

    fix gitignore

--- app/src/main/java/com/mvnh/rythmap/SecretData.java
+++
@@ -1,6 +0,0 @@
-package com.mvnh.rythmap;
-
-public class SecretData {
-    public static final String JAWG_ACCESS_TOKEN = "zoJPCAjHZjk9n2ZcHEij37TCrQY27K8FzYhq6wzaYBUmdV07RLTVyxTsfx5DqEXI";
-    public static final String SERVER_URL = "melomap.fun";
-}
